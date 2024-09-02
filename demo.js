55c2ddea54c46bac9d307af846e26d0dfca6f8fa
golamsarwar98 <golamsarwar1896@gmail.com>
2024-09-03 05:06:25 +0600

    this problems solves a problem related to printing.

---
+++ demo.js
@@ -0,0 +1,43 @@
+// const computer = {
+//     brand : "lenovo",
+//     price : 30000,
+//     processor : "intel",
+//     hdd : "512gb"
+// }
+
+// const keys = Object.keys(computer);
+// console.log(keys);
+
+// const mobile = {
+//     name : "samsung",
+//     price : 25000,
+//     color : "black",
+//     camera : "12mp",
+//     isNew : true
+// }
+
+// for(const prop in mobile){
+//     console.log(prop + " : " + mobile[prop] );
+//}
+
+// const str = "Hello";
+// str[0] = "h";
+// console.log(str);
+
+// const x = "";
+// if(!x.length){
+//     console.log("Hey");
+// }
+// else{
+//     console.log("Hi");
+// }
+
+// var person = {};
+// person.name = "John";
+// person.age = 30;
+
+// const key = Object.keys(person);
+// console.log(key);
+
+let str = "Mississippi";
+console.log(str.indexOf("i", 3));
---
+++ printwindow.js
@@ -0,0 +1,5 @@
+//A function that can be used to print document in the window.
+function printCurrentPage(){
+    //Initiates the printing.
+    window.print();
+}
